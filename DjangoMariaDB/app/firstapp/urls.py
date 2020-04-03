accd288911628ac30cce0fb671853a48871fd593
VictorRdgz <47346190+VictorRdgz@users.noreply.github.com>
2020-04-02 21:39:12 -0600

    MovieAPI

--- DjangoMariaDB/app/firstapp/urls.py
+++ DjangoMariaDB/app/firstapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('client/login',views.login,name='login'),
     path('generate_password/<str:password>',views.makepassword,name='makepassword'),
-    path('users',views.users,name='users')
+    path('users',views.users,name='users'),
+    path('client/movie',views.movieApi_key,name='movieApi_key'),
+    path('movies',views.movies,name='movies')
 ]
--- DjangoMariaDB/app/firstapp/views.py
+++ DjangoMariaDB/app/firstapp/views.py
@@ -31,6 +31,27 @@ def users(request):
     respnse_data['message'] = 'Invalid Request'
     return JsonResponse(response_data, status = 403)
 
+def movies(request):
+ if request.method =='GET':
+    response_data = {}
+    response_data["movie"] = {}
+    cont = 0
+    for i in Movie.objects.all():
+        response_data["movie"][cont] = {}
+        response_data["movie"][cont]['id'] = i.movieid
+        response_data["movie"][cont]['title'] = i.movietitle
+        response_data["movie"][cont]['releaseDate'] = i.releasedate
+        response_data["movie"][cont]['imageUrl'] = i.imageurl
+        response_data["movie"][cont]['description'] = i.description
+        cont = cont + 1
+
+    response_data['result'] = 'success'
+    return JsonResponse(response_data, status = 200)
+ else:
+    response_data['result'] = 'error'
+    respnse_data['message'] = 'Invalid Request'
+    return JsonResponse(response_data, status = 403)
+
 def login(request):
 
     #VALIDATE METHOD
@@ -118,3 +139,103 @@ def makepassword(request,password):
     response_data = {}
     response_data['password'] = hashPassword
     return JsonResponse(response_data, status=200)
+
+
+
+def movieApi_key(request):
+
+        #VALIDATE METHOD
+        if request.method == 'POST':
+
+
+            #DECLARE RESPONSE
+            response_data = {}
+            functionCheck = checkJson()
+            check_Json = functionCheck.isJson(request.body)
+            fucnction_Api_Key = ApiKey()
+
+
+
+            #CHECK JSON STRUCTURE
+            if check_Json == True:
+                json_data = json.loads(request.body)
+                check_Error = False
+                message_Error = ""
+
+                apikey = request.headers["user-api-key"]
+
+
+
+                if apikey == None:
+                    response_data = {}
+                    response_data['result'] = 'error'
+                    response_data['message'] = 'user-api-key is required'
+                    return response_data
+                else:
+                    #Checa si los parametros enviados son correctos
+                    if 'user' not in json_data:
+                        check_Error = True
+                        message_Error = "User is required"
+                    elif 'password' not in json_data:
+                        check_Error = True
+                        message_Error = "Password is required"
+                    if check_Error == True:
+                        response_data['result'] = 'error'
+                        response_data['message'] = message_Error
+                        return JsonResponse(response_data, status=401)
+                    else:
+                        #Con un try se valida si el usario es correcto de lo contrario sale el mensaje de error
+                        try:
+                            usr = json_data.get("user")
+                            usr = ApiUsers.objects.get(user=usr)
+                        except ApiUsers.DoesNotExist:
+                            response_data['result'] = 'error'
+                            response_data['message'] = 'User Doesn´t Exist or the Password is Incorrect'
+                            return JsonResponse(response_data, status=401)
+
+                        #Se obtiene el usuario junto el hash para poder validar
+                        usr = json_data.get("user")
+                        pswd = json_data.get("password")
+                        user_in_turn = ApiUsers.objects.get(user=usr)
+                        codify_password = user_in_turn.password
+
+
+                        #Funcion que recibe el hash y la contraseña escrita y las compara
+                        function_checkPassword = check_password(pswd, codify_password)
+
+                        #Si es correcto entra si no te sale el mensaje de error
+                        if function_checkPassword == True:
+                            user_apikey = request.headers["user-api-key"]
+                            current_apikey = user_in_turn.api_key
+                            if current_apikey == user_apikey:
+                                response_data = {}
+                                response_data["movie"] = {}
+                                cont = 0
+                                for i in Movie.objects.all():
+                                    response_data["movie"][cont] = {}
+                                    response_data["movie"][cont]['id'] = i.movieid
+                                    response_data["movie"][cont]['title'] = i.movietitle
+                                    response_data["movie"][cont]['releaseDate'] = i.releasedate
+                                    response_data["movie"][cont]['imageUrl'] = i.imageurl
+                                    response_data["movie"][cont]['description'] = i.description
+                                    cont = cont + 1
+
+                                response_data['result'] = 'success'
+                                return JsonResponse(response_data, status = 200)
+                            else:
+                                response_data['result'] = 'error'
+                                response_data['message'] = current_apikey
+                                return JsonResponse(response_data, status=401)
+
+
+                        else:
+                            response_data['result'] = 'error'
+                            response_data['message'] = 'User Doesn´t Exist or the Password is Incorrect'
+                            return JsonResponse(response_data, status=401)
+
+
+        else:
+            response_data = {}
+            response_data['result'] = 'error'
+            response_data['message'] = 'Invalid Request'
+            return JsonResponse(responseData, status=400)
